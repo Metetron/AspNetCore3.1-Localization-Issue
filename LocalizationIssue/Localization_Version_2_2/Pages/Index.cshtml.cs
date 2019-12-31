0d69d73f94cfbe4ab2df894c211408d756c651de
Daniel Freydl <daniel.freydl@gmail.com>
2019-12-31 21:16:37 +0100

    create new project that targets netcoreapp2.2

---
+++ LocalizationIssue/Localization_Version_2_2/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Localization_Version_2_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
